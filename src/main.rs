1b2c84f4f0116cfd55ffe138ef2efea1f83db8e2
JGDoerrer <jg.doerrer@gmail.com>
2023-10-15 11:54:00 +0200

    a bit faster

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use std::collections::HashMap;
 
-use poset::Poset;
+use poset::{Poset, MAX_N};
 
 mod poset;
 
 fn main() {
     let mut cache = HashMap::new();
-    for n in 0.. {
+    for n in 0..=MAX_N as u8 {
         for i in 0..(n + 1) / 2 {
-            if let Some((comps, tree)) = search_1(Poset::new(n), i, &mut cache) {
-                println!("n = {n}, i = {i}, comps = {comps}");
-                // println!("tree = {tree:?}");
+            if let Some(comparisions) = search(Poset::new(n, i), u8::MAX, &mut cache) {
+                println!("n = {n}, i = {i}, comparisions = {comparisions}");
+                println!("cache_entries = {}", cache.len());
             } else {
             }
         }
@@ -20,82 +20,22 @@ fn main() {
 #[derive(Debug, Clone)]
 enum DecisionTree {
     Compare(u8, u8, Box<DecisionTree>, Box<DecisionTree>),
-    End(u8),
+    Return(u8),
 }
 
-fn search_1(
-    poset: Poset,
-    index: u8,
-    cache: &mut HashMap<(Poset, u8), Option<(u8, DecisionTree)>>,
-) -> Option<(u8, DecisionTree)> {
-    if let Some(result) = cache.get(&(poset.clone(), index)) {
-        return result.clone();
+fn search(poset: Poset, mut max_comps: u8, cache: &mut HashMap<Poset, Option<u8>>) -> Option<u8> {
+    if let Some(result) = cache.get(&poset) {
+        return *result;
     }
 
-    // dbg!(&poset.n(), index, comparisons);
-    if let Some(i) = poset.get_ith_element(index) {
-        cache.insert((poset, index), Some((0, DecisionTree::End(i))));
-        return Some((0, DecisionTree::End(i)));
+    if poset.n() == 1 {
+        cache.insert(poset.clone(), Some(0));
+
+        return Some(0);
     }
 
-    for j in 0..index {
-        if let Some(i) = poset.get_ith_element(j) {
-            // dbg!(i);
-
-            // for i in 0..poset.n() {
-            //     for j in 0..poset.n() {
-            //         if poset.is_less((i, j)) {
-            //             println!("{i} < {j}")
-            //         }
-            //     }
-            // }
-            // dbg!(&poset);
-            // println!("---");
-
-            let poset = poset.reduce(i);
-
-            // for i in 0..poset.n() {
-            //     for j in 0..poset.n() {
-            //         if poset.is_less((i, j)) {
-            //             println!("{i} < {j}")
-            //         }
-            //     }
-            // }
-            // dbg!(&poset);
-            // println!("+++");
-
-            return search_1(poset, index - 1, cache);
-        }
-    }
-
-    for j in (index + 1)..poset.n() {
-        if let Some(i) = poset.get_ith_element(j) {
-            // dbg!(i);
-
-            // for i in 0..poset.n() {
-            //     for j in 0..poset.n() {
-            //         if poset.is_less((i, j)) {
-            //             println!("{i} < {j}")
-            //         }
-            //     }
-            // }
-            // dbg!(&poset);
-            // println!("---");
-
-            let poset = poset.reduce(i);
-
-            // for i in 0..poset.n() {
-            //     for j in 0..poset.n() {
-            //         if poset.is_less((i, j)) {
-            //             println!("{i} < {j}")
-            //         }
-            //     }
-            // }
-            // dbg!(&poset);
-            // println!("+++");
-
-            return search_1(poset, index, cache);
-        }
+    if max_comps == 0 {
+        return None;
     }
 
     let mut result = None;
@@ -106,70 +46,28 @@ fn search_1(
                 continue;
             }
 
-            let (less, greater) = poset.do_comparison((i, j));
+            let less = poset.with_less((i, j));
+            let less_result = search(less, max_comps - 1, cache);
 
-            if poset.is_valid_less((i, j)) && !poset.is_valid_less((j, i)) {
-                let less_result = search_1(less, index, cache);
+            let greater = poset.with_less((j, i));
+            let greater_result = search(greater, max_comps - 1, cache);
 
-                if let Some((comps_less, dt_less)) = less_result {
-                    if let Some((min, min_dt)) = &mut result {
-                        if *min > comps_less {
-                            *min = comps_less;
-                            *min_dt = dt_less
-                        }
+            if let Some(comps_less) = less_result {
+                if let Some(comps_greater) = greater_result {
+                    let comps_max = comps_less.max(comps_greater) + 1;
+                    max_comps = max_comps.min(comps_max);
+
+                    if let Some(min) = &mut result {
+                        *min = comps_max.min(*min);
                     } else {
-                        result = Some((comps_less, dt_less))
-                    }
-                }
-            } else if !poset.is_valid_less((i, j)) && poset.is_valid_less((j, i)) {
-                let greater_result = search_1(greater, index, cache);
-
-                if let Some((comps_greater, dt_greater)) = greater_result {
-                    if let Some((min, min_dt)) = &mut result {
-                        if *min > comps_greater {
-                            *min = comps_greater;
-                            *min_dt = dt_greater
-                        }
-                    } else {
-                        result = Some((comps_greater, dt_greater))
-                    }
-                }
-            } else {
-                let less_result = search_1(less, index, cache);
-                let greater_result = search_1(greater, index, cache);
-
-                if let Some((comps_less, dt_less)) = less_result {
-                    if let Some((comps_greater, dt_greater)) = greater_result {
-                        let comps_max = comps_less.max(comps_greater) + 1;
-
-                        if let Some((min, min_dt)) = &mut result {
-                            if *min > comps_max {
-                                *min = comps_max;
-                                *min_dt = DecisionTree::Compare(
-                                    i,
-                                    j,
-                                    Box::new(dt_less),
-                                    Box::new(dt_greater),
-                                )
-                            }
-                        } else {
-                            result = Some((
-                                comps_max,
-                                DecisionTree::Compare(
-                                    i,
-                                    j,
-                                    Box::new(dt_less),
-                                    Box::new(dt_greater),
-                                ),
-                            ))
-                        }
+                        result = Some(comps_max)
                     }
                 }
             }
         }
     }
 
-    cache.insert((poset, index), result.clone());
+    cache.insert(poset, result);
 
     result
 }
--- src/poset.rs
+++ src/poset.rs
@@ -1,20 +1,27 @@
 use std::fmt::Debug;
 
+pub const MAX_N: usize = 15;
+
 /// A partially ordered set with <
 #[derive(Clone, PartialEq, Eq, Hash)]
 pub struct Poset {
+    /// The number of elements
     n: u8,
-    adjacency: Vec<u8>,
+    i: u8,
+    adjacency: [u8; Self::BYTES],
 }
 
 impl Poset {
-    pub fn new(n: u8) -> Self {
-        let bits = n as usize * n as usize;
-        let bytes = (bits + 7) / 8;
+    const BITS: usize = MAX_N * (MAX_N - 1) / 2;
+    const BYTES: usize = (Self::BITS + 7) / 8;
+
+    pub fn new(n: u8, i: u8) -> Self {
+        debug_assert!(n < MAX_N as u8);
 
         Poset {
             n,
-            adjacency: vec![0; bytes],
+            i,
+            adjacency: [0; Self::BYTES],
         }
     }
 
@@ -22,130 +29,168 @@ impl Poset {
         self.n
     }
 
-    /// i < j
-    pub fn add_less(&self, (i, j): (u8, u8)) -> Self {
-        let bit = i + j * self.n;
+    pub fn i(&self) -> u8 {
+        self.i
+    }
+
+    fn normalize(&mut self) {
+        // how many elements are less than it
+        let mut less = [0; MAX_N];
+        // how many elements are greater than it
+        let mut greater = [0; MAX_N];
+
+        for i in 0..self.n {
+            for j in 0..self.n {
+                if self.is_less(i, j) {
+                    less[j as usize] += 1;
+                    greater[i as usize] += 1;
+                }
+            }
+        }
+
+        // can the element be ignored, because it is too large/small
+        let mut dropped = [false; MAX_N];
+        let mut n_less_dropped = 0;
+
+        for i in 0..self.n as usize {
+            if greater[i] > self.i {
+                dropped[i] = true;
+            } else if less[i] >= self.n - self.i {
+                dropped[i] = true;
+                n_less_dropped += 1;
+            }
+        }
+
+        // maps the old indices to the new ones
+        let mut new_indices = [0; MAX_N];
+        let mut new_n = 0;
+        let mut b = self.n as usize - 1;
+
+        for i in 0..self.n {
+            if !dropped[i as usize] {
+                new_indices[new_n as usize] = i;
+                new_n += 1;
+            } else {
+                new_indices[b] = i;
+                b -= 1;
+            }
+        }
+
+        // recalculate less/greater for the new indices
+        if new_n != self.n as usize {
+            for i in 0..new_n {
+                for j in 0..new_n {
+                    if self.is_less(new_indices[i], new_indices[j]) {
+                        less[j as usize] += 1;
+                        greater[i as usize] += 1;
+                    }
+                }
+            }
+        }
+
+        // a heuristic to sort the elements
+        let mut lessness = [0; MAX_N];
+
+        for i in 0..new_n {
+            let new_index = new_indices[i] as usize;
+            lessness[new_index] =
+                MAX_N * MAX_N - greater[new_index] as usize * MAX_N - less[new_index] as usize;
+        }
+
+        new_indices[0..self.n as usize]
+            .sort_by(|a, b| lessness[*a as usize].cmp(&lessness[*b as usize]).reverse());
+
+        let mut new = Poset::new(new_n as u8, self.i - n_less_dropped);
+
+        // make the new poset
+        for i in 0..new.n {
+            for j in 0..new.n {
+                if self.is_less(new_indices[i as usize], new_indices[j as usize]) {
+                    new.set_bit(i, j)
+                }
+            }
+        }
+
+        // dbg!(&self, &new);
+        debug_assert!(new.is_closed());
+        *self = new;
+    }
+
+    fn get_bit_index(&self, i: u8, j: u8) -> u8 {
+        i + j * (self.n - 1) - if i > j { 1 } else { 0 }
+    }
+
+    fn set_bit(&mut self, i: u8, j: u8) {
+        debug_assert!(i != j);
+        let bit = self.get_bit_index(i, j);
         let byte = bit / 8;
         let mask = 1 << (bit % 8);
 
-        let mut new = self.clone();
-        new.adjacency[byte as usize] |= mask;
-        new
+        self.adjacency[byte as usize] |= mask;
     }
 
     /// is i < j?
-    pub fn is_less(&self, (i, j): (u8, u8)) -> bool {
-        let bit = i + j * self.n;
+    pub fn is_less(&self, i: u8, j: u8) -> bool {
+        if i == j {
+            return false;
+        }
+
+        let bit = self.get_bit_index(i, j);
         let byte = bit / 8;
         let mask = 1 << (bit % 8);
 
         (self.adjacency[byte as usize] & mask) > 0
     }
 
-    /// is i < ... < j?
-    pub fn is_less_rec(&self, (i, j): (u8, u8)) -> bool {
-        if self.is_less((i, j)) {
-            true
-        } else {
-            for k in 0..self.n {
-                if self.is_less((i, k)) && self.is_less_rec((k, j)) {
-                    return true;
-                }
-            }
-
-            false
-        }
-    }
-
-    pub fn has_order(&self, (i, j): (u8, u8)) -> bool {
-        self.is_less((i, j)) || self.is_less((j, i))
-    }
-
-    pub fn is_valid_less(&self, (i, j): (u8, u8)) -> bool {
-        !self.is_less_rec((j, i))
-    }
-
-    pub fn is_valid_comp(&self, (i, j): (u8, u8)) -> bool {
-        self.is_valid_less((j, i)) && self.is_valid_less((j, i))
-    }
-
-    pub fn get_ith_element(&self, i: u8) -> Option<u8> {
-        for j in 0..self.n {
-            let less_count = self.elements_less_than(j);
-            let greater_count = self.elements_greater_than(j);
-
-            if less_count == i && less_count + greater_count + 1 == self.n {
-                return Some(j);
-            }
-        }
-
-        None
-    }
-
-    pub fn elements_less_than(&self, index: u8) -> u8 {
-        let mut count = 0;
-        for i in 0..self.n {
-            if self.is_less((i, index)) {
-                count += 1;
-            }
-        }
-        count
-    }
-
-    pub fn elements_greater_than(&self, index: u8) -> u8 {
-        let mut count = 0;
-        for i in 0..self.n {
-            if self.is_less((index, i)) {
-                count += 1;
-            }
-        }
-        count
-    }
-
-    pub fn next_posets(&self) -> Vec<Poset> {
-        let mut posets = vec![];
-
+    /// for debugging
+    fn is_closed(&self) -> bool {
         for i in 0..self.n {
             for j in 0..self.n {
-                if i == j || self.is_less((j, i)) {
-                    continue;
-                }
-
-                let pair = (i, j);
-                if !self.is_less(pair) {
-                    posets.push(self.add_less(pair));
+                for k in 0..self.n {
+                    if self.is_less(i, j) && self.is_less(j, k) && !self.is_less(i, k) {
+                        return false;
+                    }
                 }
             }
         }
 
-        posets
+        true
     }
 
-    pub fn do_comparison(&self, (i, j): (u8, u8)) -> (Poset, Poset) {
-        let less = self.add_less((i, j));
-        let greater = self.add_less((j, i));
+    /// adds i < j to the poset
+    pub fn add_less(&mut self, i: u8, j: u8) {
+        debug_assert!(!self.is_less(i, j));
 
-        (less, greater)
+        self.close(i, j);
+        self.normalize();
+
+        debug_assert!(self.is_closed(), "{self:?}");
     }
 
-    pub fn reduce(&self, i: u8) -> Self {
-        let mut new = Poset::new(self.n - 1);
-
-        for j in 0..self.n {
-            for k in 0..self.n {
-                if j == i || k == i {
-                    continue;
+    /// adds i < j and makes sure, that i < j && j < k => i < k is true
+    fn close(&mut self, i: u8, j: u8) {
+        self.set_bit(i, j);
+        for k in 0..self.n {
+            if i != k && j != k {
+                if self.is_less(k, i) {
+                    self.close(k, j)
                 }
-
-                if self.is_less((j, k)) {
-                    let j = if j > i { j - 1 } else { j };
-                    let k = if k > i { k - 1 } else { k };
-                    new = new.add_less((j, k));
+                if self.is_less(j, k) {
+                    self.close(j, k)
                 }
             }
         }
+    }
 
+    pub fn with_less(&self, (i, j): (u8, u8)) -> Self {
+        let mut new = self.clone();
+        new.add_less(i, j);
         new
+    }
+
+    /// is either i < j or j < i?
+    pub fn has_order(&self, (i, j): (u8, u8)) -> bool {
+        self.is_less(i, j) || self.is_less(j, i)
     }
 }
 
@@ -155,51 +200,32 @@ impl Debug for Poset {
         let adjacency: Vec<String> = (0..self.n)
             .map(|i| {
                 (0..self.n)
-                    .map(|j| if self.is_less((i, j)) { '1' } else { '0' })
+                    .map(|j| if self.is_less(i, j) { '1' } else { '0' })
                     .collect()
             })
+            .collect();
+        let comparisons: Vec<String> = (0..self.n)
+            .map(|i| {
+                let vecs: Vec<String> = (0..self.n)
+                    .map(|j| {
+                        if self.is_less(i, j) {
+                            vec![format!("{i} < {j}")]
+                        } else {
+                            vec![]
+                        }
+                    })
+                    .flatten()
+                    .collect();
+                vecs
+            })
+            .flatten()
             .collect();
 
         f.debug_struct("Poset")
             .field("n", &self.n)
+            .field("i", &self.i)
             .field("adjacency", &adjacency)
+            .field("comparisons", &comparisons)
             .finish()
-    }
-}
-
-#[cfg(test)]
-mod test {
-
-    use super::*;
-
-    #[test]
-    fn test_less_rec() {
-        let poset = Poset::new(5)
-            .add_less((0, 1))
-            .add_less((1, 2))
-            .add_less((2, 3));
-
-        assert!(poset.is_less_rec((0, 3)));
-        assert!(poset.is_less_rec((1, 3)));
-        assert!(poset.is_less_rec((0, 2)));
-    }
-
-    #[test]
-    fn test_valid_comparison() {
-        let poset = Poset::new(3).add_less((0, 1)).add_less((1, 2));
-
-        assert!(!poset.is_valid_comp((0, 2)));
-
-        let poset = Poset::new(3).add_less((0, 1)).add_less((2, 0));
-
-        assert!(!poset.is_valid_comp((2, 1)));
-
-        let poset = Poset::new(5)
-            .add_less((0, 1))
-            .add_less((1, 2))
-            .add_less((2, 3))
-            .add_less((3, 4));
-
-        assert!(!poset.is_valid_comp((0, 4)));
     }
 }
