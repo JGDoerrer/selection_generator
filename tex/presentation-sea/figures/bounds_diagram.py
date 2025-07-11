29c9b2c8ceec1e864a1ad8817aff75ee687eb7ee
st180465 <st180465@stud.uni-stuttgart.de>
2025-07-11 10:38:10 +0200

    improve figure annotations

--- tex/presentation-sea/figures/bounds_diagram.py
+++ tex/presentation-sea/figures/bounds_diagram.py
@@ -5,27 +5,37 @@ import numpy as np
 n = np.linspace(1, 100, 500)  # Sample n values
 
 # Plot lines
-plt.figure(figsize=(24,16))
-plt.plot(n, n - 1, label='Selecting smallest: $n-1$', linewidth=5)
-plt.plot(n, 2.41 * n, label='Median lower bound: $2.41n$', linewidth=5)
+plt.figure(figsize=(35,16))
 plt.plot(n, 2.95 * n, label='Median upper bound: $2.95n$', linewidth=5)
+plt.plot(n, 2.41 * n, label='Median lower bound: $2.41n$', linewidth=5)
+plt.plot(n, n - 1, label='Selecting smallest: $n-1$', linewidth=5)
 
 # Shade the gap between lower and upper bounds
+plt.fill_between(n, n - 1, 2.41*n, alpha=0.2, color='orange')
 plt.fill_between(n, 2.41 * n, 2.95 * n, alpha=0.2, color='yellow')
 
 # Annotations
-mid_n = 50
-mid_val = (2.41 + 2.95) / 2 * mid_n
+mid_n = 95
+mid_val =250
 plt.annotate('Gap between bounds',
              xy=(mid_n, mid_val),
              xytext=(mid_n + 10, mid_val + 20),
              arrowprops=dict(arrowstyle='->',color='black', linewidth=4),
-             fontsize=44,
+             fontsize=55,
+             )
+
+mid_n = 95
+mid_val = 150
+plt.annotate('Gap between i=0 and median',
+             xy=(mid_n, mid_val),
+             xytext=(mid_n + 10, mid_val + 20),
+             arrowprops=dict(arrowstyle='->',color='black', linewidth=4),
+             fontsize=55,
              )
 
 # Labels and legend
-plt.xlabel('Input size $n$', fontsize=50, labelpad=40)
-plt.ylabel('Number of comparisons', fontsize=50, labelpad=40)
+plt.xlabel('Input size $n$', fontsize=60, labelpad=40)
+plt.ylabel('Number of comparisons', fontsize=60, labelpad=40)
 plt.tick_params(axis='both', length=18, width=3, labelsize=40)
 
 plt.legend(fontsize=48)
