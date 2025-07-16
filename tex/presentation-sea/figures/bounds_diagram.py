670fb063e4e8778298c000d2aa0f2ff92cf20ec3
st180465 <st180465@stud.uni-stuttgart.de>
2025-07-16 08:53:21 +0200

    clarify conjectured lower bound

--- tex/presentation-sea/figures/bounds_diagram.py
+++ tex/presentation-sea/figures/bounds_diagram.py
@@ -5,30 +5,30 @@ import numpy as np
 n = np.linspace(0, 21, 100)  # Sample n values
 
 # Plot lines
-plt.figure(figsize=(35,16))
-plt.plot(n, 2.95 * n, label='Median upper bound: $2.95n$', linewidth=5)
-plt.plot(n, 2.41 * n, label='Median lower bound: $2.41n$', linewidth=5)
+plt.figure(figsize=(42,16))
+plt.plot(n, 2.95 * n, label='Upper bound for the median: $2.95n$', linewidth=5)
+plt.plot(n, 2* n, label='Lower bound for the median: $2n - \mathcal{o}(n)$', linewidth=5)
+plt.plot(n, 2.41 * n, label='Conjectured lower bound for the median: $2.41n$', linestyle='--', color='orange',linewidth=5 )
 plt.plot(n, n - 1, label='Selecting smallest: $n-1$', linewidth=5)
 
 # Shade the gap between lower and upper bounds
-plt.fill_between(n, n - 1, 2.41*n, alpha=0.2, color='orange')
-plt.fill_between(n, 2.41 * n, 2.95 * n, alpha=0.2, color='yellow')
+plt.fill_between(n, n - 1, 2*n, alpha=0.2, color='orange')
+plt.fill_between(n, 2* n, 2.95 * n, alpha=0.2, color='yellow')
 
 # Annotations
-mid_n = 19
-mid_val = 50
-plt.annotate('Gap between asymptotic bounds',
+mid_n = 20
+mid_val = 52
+plt.annotate('Gap between\nasymptotic bounds',
              xy=(mid_n, mid_val),
-             xytext=(mid_n + 2, mid_val + 4),
+             xytext=(mid_n + 2.5, mid_val+4),
              arrowprops=dict(arrowstyle='->',color='black', linewidth=4),
              fontsize=55,
              )
 
-mid_n = 19
-mid_val = 25
-plt.annotate('Gap between i=1 and median',
+mid_val = 27
+plt.annotate('Gap between i=1\nand median',
              xy=(mid_n, mid_val),
-             xytext=(mid_n + 2, mid_val + 4),
+             xytext=(mid_n + 2.5, mid_val),
              arrowprops=dict(arrowstyle='->',color='black', linewidth=4),
              fontsize=55,
              )
@@ -37,9 +37,10 @@ plt.annotate('Gap between i=1 and median',
 plt.xlabel('Input size $n$', fontsize=60, labelpad=40)
 plt.ylabel('Number of comparisons', fontsize=60, labelpad=40)
 plt.tick_params(axis='both', length=18, width=3, labelsize=40)
-plt.xticks(np.arange(0, 21, 5), fontsize=40)
+plt.xticks([], labels=[], fontsize=40)
+plt.yticks([])
 plt.legend(fontsize=48)
 plt.tight_layout()
 
 # Display
-plt.savefig("tex/presentation-sea/figures/bounds_diagram.png")
+plt.savefig("tex/presentation-sea/figures/bounds_diagram.png", bbox_inches='tight')
